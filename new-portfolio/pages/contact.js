3746acb4fbf792d3e51e8eb5eff19626df2ac225
Ryan Spears <spearswesley5@gmail.com>
2022-05-25 17:00:16 -0500

    Escape characters

--- new-portfolio/pages/contact.js
+++ new-portfolio/pages/contact.js
@@ -11,11 +11,11 @@ const contact = () => {
         <h2>Need to get in touch with me?</h2>
         <h3>Fill out the form below, and I'll get back with you.</h3>
         <form className={contactStyles.form} action={'https://formspree.io/f/mlezzjqe'} method={'POST'}>
-          <input type='text' name='name' placeholder='Name' />
-          <input type='email' name='email' placeholder='Email' />
-          <input type='tel' name='phone' placeholder='Phone Number' />
-          <textarea name='message' placeholder='Message' />
-          <button type='submit'>Send</button>
+          <input type={'text'} name={'name'} placeholder={'Name'} />
+          <input type={'email'} name={'email'} placeholder={'Email'} />
+          <input type={'tel'} name={'phone'} placeholder={'Phone Number'} />
+          <textarea name={'message'} placeholder={'Message'} />
+          <button type={'submit'}>Send</button>
         </form>
     </div>
   )
