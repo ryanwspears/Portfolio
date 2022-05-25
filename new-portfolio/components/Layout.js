c25547cf36bd17dc158883c1d96d3cdac1c87eef
Ryan Spears <spearswesley5@gmail.com>
2022-05-25 15:21:24 -0500

    Fixed Responsiveness

--- new-portfolio/components/Layout.js
+++ new-portfolio/components/Layout.js
@@ -3,6 +3,7 @@ import styles from '../styles/Layout.module.css'
 import Nav from './Nav'
 import Header from './Header'
 import Footer from './Footer'
+import Head from 'next/head'
 
 const Layout = ({children}) => {
   return (
@@ -10,6 +11,9 @@ const Layout = ({children}) => {
         <Header />
         <Nav />
         <div className={styles.container}>
+            <Head>
+              <meta content="width=device-width, initial-scale=1" name="viewport" />
+            </Head>
             <main className={styles.main}>
                 {children}
             </main>
