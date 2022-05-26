cf2cd9b92a87f4d477b14de1350b38b3bdcf4466
ryanwspears <spearswesley5@gmail.com>
2022-05-25 21:20:44 -0500

    Update index.js

--- new-portfolio/pages/index.js
+++ new-portfolio/pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
       </Head>
       <div className={homeStyles.text}>
         <Typist>
-          <h1>var <span className={homeStyles.var}>suprise</span> = "It seems you{"\'"}ve stumbled upon my portfolio...";</h1>
+          <h1>var <span className={homeStyles.var}>suprise</span> = {'\"'}It seems you{"\'"}ve stumbled upon my portfolio...{'\"'};</h1>
           <Typist.Delay ms={500} />
-          <h1 className={homeStyles.line__2}>const <span className={homeStyles.var}>introduction</span> = "My name is Ryan Spears, and I{"\'"}m a Software Developer from Jackson, MS.";</h1>
+          <h1 className={homeStyles.line__2}>const <span className={homeStyles.var}>introduction</span> = {'\"'}My name is Ryan Spears, and I{"\'"}m a Software Developer from Jackson, MS.{'\"'};</h1>
           <Typist.Delay ms={500} />
-          <h1>let <span className={homeStyles.var}>invitation</span> = "Feel free to look around to find out more about my work.";</h1>
+          <h1>let <span className={homeStyles.var}>invitation</span> = {'\"'}Feel free to look around to find out more about my work.{'\"'};</h1>
         </Typist>
       </div>
     </div>
