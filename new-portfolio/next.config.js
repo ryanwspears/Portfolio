1660233a2eea53579fd452af5fa147acedf14dd8
= <spearswesley5@gmail.com>
2023-06-16 02:39:16 -0500

    Added Contentful CMS
    Added CMS to make managing projects easier.

--- new-portfolio/next.config.js
+++ new-portfolio/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost']
+    domains: ['localhost', 'images.ctfassets.net']
+  },
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
   }
 }
 
